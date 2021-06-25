3d9648e5e1ffbedf8dd91d01469a719deb79def4
yoziyo <j16412000kr@gmail.com>
2021-06-25 13:47:27 +0900

    bugfixed

--- ios/template/AppDelegate.m
+++ ios/template/AppDelegate.m
@@ -12,6 +12,8 @@
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
 
+#import <Firebase.h>
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
