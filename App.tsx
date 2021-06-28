975349d36549b8de5d5804432a87303eecca7e8d
yoziyo <j16412000kr@gmail.com>
2021-06-28 15:48:38 +0900

    firebase test

--- App.tsx
+++ App.tsx
@@ -61,7 +61,7 @@ const App = () => {
           }}
         >
           <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
+            HELLO LYCL TEST Edit <Text style={styles.highlight}>App.js</Text> to change this screen and then come back to see your edits.
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
