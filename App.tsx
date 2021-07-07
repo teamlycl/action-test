b51b18dd08894be0ab4b250c0625f134b4757381
raccoonkang <lacoonv@gmail.com>
2021-07-07 15:06:03 +0900

    test codepush

--- App.tsx
+++ App.tsx
@@ -71,7 +71,7 @@ const App = () => {
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}
         >
-          <Section title="Step One">1 codepush version {label}</Section>
+          <Section title="Step One">2 codepush version {label}</Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
           </Section>
